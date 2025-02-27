b4c0b8754c842faeee4eafd2bce063480eeeb675
DLOADIN <m.david@alustudent.com>
2025-02-27 18:58:11 +0200

    New One

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -2,17 +2,16 @@
 
 import { OverviewSection } from "./components/overview-section"
 import { TrendCharts } from "./components/trend-charts"
+import AccidentTable from "./components/maindatatable"
 
 export default function DashboardPage() {
   return (
     <div className="space-y-6">
       <h2 className="text-2xl font-bold text-[#1a1d1f]">Dashboard</h2>
-      
-      
-      {/* <OverviewSection />
-      <TrendCharts /> */}
-      
-    </div>
+      <AccidentTable />
+        {/* <OverviewSection />
+        <TrendCharts /> */}
+      </div>
     
   )
 }
