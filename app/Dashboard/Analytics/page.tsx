0eb45667c29258e3e46cd6d6876c858a9186da59
DLOADIN <m.david@alustudent.com>
2025-03-03 21:17:13 +0200

    New One

--- app/Dashboard/Analytics/page.tsx
+++ app/Dashboard/Analytics/page.tsx
@@ -5,7 +5,7 @@ import AccidentDataChart from "../components/notifications-table"
 export default function NotificationsPage() {
   return (
       <div className="space-y-6">
-        <h2 className="text-3xl font-bold tracking-tight">Notifications</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Analytics</h2>
         <AccidentDataChart />
       </div>
   )
