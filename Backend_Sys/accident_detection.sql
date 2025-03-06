0691806d45935a1c44697a102539d982efa25c5d
DLOADIN <m.david@alustudent.com>
2025-03-07 00:14:06 +0200

    paper

--- Backend_Sys/accident_detection.sql
+++ Backend_Sys/accident_detection.sql
@@ -1,13 +1,12 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 
-CREATE TABLE `accidents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` datetime NOT NULL,
-  `location` varchar(255) NOT NULL,
-  `severity_level` enum('low','medium','high') NOT NULL,
-  `severity_score` float NOT NULL,
-  `video_path` varchar(255) NOT NULL,
-  `accuracy` float NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE accidents (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
+    location VARCHAR(255),
+    severity_level VARCHAR(50),
+    severity_score FLOAT,
+    video_path VARCHAR(255),
+    accuracy FLOAT
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
